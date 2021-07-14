607c9feffd5f394cd86b6163d48d0593d51d7aa4
JuCaLG <49450909+JuCaLG@users.noreply.github.com>
2021-07-14 18:18:56 -0500

    ProyectoCompleto

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppNavigation from '@navigation/AppNavigation'
+import { UsuarioProvider } from '@context/UsuarioContext'
+//import MyDrawer from './menu';
+
+export default function App() {
+  return (
+    <UsuarioProvider>
+      <AppNavigation />
+    </UsuarioProvider>
+
+  )
+}
